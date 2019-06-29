23bfb9250cd7d6b6f3675508f9c4a625c8735d5e
Junki0122 <cs182018@meiji.ac.jp>
2019-06-29 16:32:36 +0900

    feat: firestoreとの連携

--- functions/api/index.js
+++ functions/api/index.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const serviceAccount = require('../config/serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://sample-1aae6.firebaseio.com"
+});
 
 const app = express();
+const fireStore = admin.firestore();
+
+app.get('/hellos', (req, res, next) => {
+  fireStore.collection('hellos').get()
+    // eslint-disable-next-line promise/always-return
+    .then(snapshot => {
+      let responses = [];
+      snapshot.forEach(doc => {
+        responses.push(doc.data());
+      });
+      res.json(responses);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
 app.get('/hellos/japan', (req, res) => {
   res.send('こんにちは');
